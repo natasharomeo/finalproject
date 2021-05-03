618e799d301573e6788d63e29b64a67e16b5a360
natasharomeo <you@example.com>
2021-05-03 21:31:58 +1000

    final look

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
-<html>
-<head>
-<title>Title of the document</title>
-</head>
+<html lang="en">
+    <head>
+        <title>Cycling without Age</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body class="bg-gray-200">
+        <nav class="p-6 bg-white flex justify-between mb-6">
+            <ul class="flex items-center">
+                <li>
+                    <a href="/" class="p-3">Home</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Dashboard</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Posts</a>
+                </li>
+            </ul>
 
-<body>
-@yield('content')
-</body>
-
+            <ul class="flex items-center">
+                    <li>
+                        <a href="" class="p-3">Login</a>
+                    </li>
+                    <li>
+                        <a href="" class="p-3">Register</a>
+                    </li>
+            </ul>
+        </nav>
+        @yield('content')
+    </body>
 </html>
