5d12457cc27d0bb76473bfad592319e0c8327ae8
natasharomeo <you@example.com>
2021-05-04 21:30:24 +1000

    design layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,26 +4,20 @@
         <title>Cycling without Age</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body class="bg-gray-200">
+    <body class="bg-gray-100">
         <nav class="p-6 bg-white flex justify-between mb-6">
             <ul class="flex items-center">
                 <li>
-                    <a href="/" class="p-3">Home</a>
-                </li>
-                <li>
-                    <a href="" class="p-3">Dashboard</a>
-                </li>
-                <li>
-                    <a href="" class="p-3">Posts</a>
+                    <a href="/" class="p-3">Cycling Without Age</a>
                 </li>
             </ul>
 
             <ul class="flex items-center">
                     <li>
-                        <a href="" class="p-3">Login</a>
+                        <a href="" class="p-3">User Name</a>
                     </li>
                     <li>
-                        <a href="" class="p-3">Register</a>
+                        <a href="" class="p-3">Logout</a>
                     </li>
             </ul>
         </nav>
