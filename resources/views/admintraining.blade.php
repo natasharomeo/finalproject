f4ca45dc601580f39e44f5355a601df61c0c808a
PrxnceC98 <18982624@student.westernsydney.edu.au>
2021-05-12 10:53:41 +1000

    Update

--- resources/views/admintraining.blade.php
+++ resources/views/admintraining.blade.php
@@ -23,7 +23,7 @@
                     <tr class="border-b hover:bg-orange-100 bg-gray-100">
                     <td class="p-3 px-5"><input type="text" value="Name" class="bg-transparent"></td>
                     <td class="p-3 px-5"><input type="text" value="Location" class="bg-transparent"></td>
-                    <td class="p-3 px-5"><input type="text" value="Distance" class="bg-transparent"></td>
+                    <td class="p-3 px-5"><input type="int" value="Distance" class="bg-transparent"></td>
                     <td class="p-3 px-5"><input type="text" value="Ride Leader" class="bg-transparent"></td>
                     <td class="p-3 px-5"><input type="date" value="Date" class="bg-transparent"></td>
                     <td class="p-3 px-5 flex justify-end"><button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Save</button><button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button></td>
