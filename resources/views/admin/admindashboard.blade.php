e34b8f2b3c39cbbf2a554bfda4bd343272ee7290
PrxnceC98 <18982624@student.westernsydney.edu.au>
2021-06-01 23:12:29 +1000

    Admin dashboard updated

--- resources/views/admin/admindashboard.blade.php
+++ resources/views/admin/admindashboard.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.layouts')
 
 @section('content')
-         
 
+<h1 style="color:red;" class="text-center md:font-bold text-3xl  pb-4">
+Welcome to your Administration dashboard
+</h1>
 
 
 
